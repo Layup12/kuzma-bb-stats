8bb39328b9e4badf850846e0fadcc98d36677d51
Валерий К <layup@MacBook-Pro-Valerij.local>
2022-11-28 04:51:59 +0500

    deleted env files, testing libs

--- src/app.module.ts
+++ src/app.module.ts
@@ -14,6 +14,7 @@ import { ServeStaticModule } from '@nestjs/serve-static';
 import { PlayersModule } from './players/players.module';
 import * as path from 'path';
 import { Player } from './players/players.model';
+
 @Module({
   controllers: [],
   providers: [],
